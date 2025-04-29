37f0d6fa8214884796d7488ddf693b1dc48cccda
Channappa Jagadish <channappa@gmail.com>
2025-04-29 21:10:32 +0530

    CJ - authored test for "add to cart"

--- cypress/e2e/basic-shopping.cy.js
+++ cypress/e2e/basic-shopping.cy.js
@@ -1,41 +1,93 @@
 describe('Basic Shopping Experience', () => {
-  it('adds multiple products to cart', () => {
+  const sunglassesPrice = '$19.99';
+  const watchPrice = '$109.99';
+  
+  it('adds Watch and Sunglasses to cart and verifies total', () => {
     navigateToHomePage();
-    selectFirstProduct();
+    selectSunglasses();
     addToCart();
     returnToShopping();
-    selectSecondProduct();
+    selectWatch();
     addToCart();
-    verifyCartHasItems();
+    verifyCartContentsAndTotal();
   });
   
   function navigateToHomePage() {
     cy.visit('http://localhost:8080');
-    cy.contains('Hot Products').should('exist');
+    cy.get('.navbar-brand img.top-left-logo').should('be.visible');
+    cy.get('.hot-products-row h3').should('contain', 'Hot Products');
+    
+    cy.get('.hot-product-card').should('have.length.at.least', 3);
   }
   
-  function selectFirstProduct() {
-    cy.get('.hot-product-card a').first().click();
+  function selectSunglasses() {
+    cy.get('.hot-product-card')
+      .contains('.hot-product-card-name', 'Sunglasses')
+      .parents('.hot-product-card')
+      .within(() => {
+        cy.get('.hot-product-card-price').should('contain', sunglassesPrice);
+        cy.get('a').click();
+      });
+    
     cy.url().should('include', '/product/');
+    
+    cy.contains('Sunglasses').should('be.visible');
+    cy.contains(sunglassesPrice).should('be.visible');
+  }
+  
+  function selectWatch() {
+    cy.get('.hot-product-card')
+      .contains('.hot-product-card-name', 'Watch')
+      .parents('.hot-product-card')
+      .within(() => {
+        cy.get('.hot-product-card-price').should('contain', watchPrice);
+        cy.get('a').click();
+      });
+    
+    cy.url().should('include', '/product/');
+    
+    cy.contains('Watch').should('be.visible');
+    cy.contains(watchPrice).should('be.visible');
   }
   
   function addToCart() {
-    cy.get('button[type="submit"]').click();
+    cy.get('button[type="submit"]')
+      .should('be.visible')
+      .should('be.enabled')
+      .click();
+    
     cy.url().should('include', '/cart');
   }
   
   function returnToShopping() {
     cy.get('a').contains('Continue Shopping').click();
-    cy.url().should('not.include', '/cart');
+    
+    cy.url().should('include', 'http://localhost:8080');
+    cy.get('.hot-products-row').should('be.visible');
   }
   
-  function selectSecondProduct() {
-    cy.get('.hot-product-card a').eq(1).click();
-    cy.url().should('include', '/product/');
-  }
-  
-  function verifyCartHasItems() {
+  function verifyCartContentsAndTotal() {
     cy.url().should('include', '/cart');
+    
     cy.get('.empty-cart-section').should('not.exist');
+    
+    cy.contains('Sunglasses').should('be.visible');
+    cy.contains('Watch').should('be.visible');
+    
+    cy.contains(sunglassesPrice).should('be.visible');
+    cy.contains(watchPrice).should('be.visible');
+    
+    cy.contains('Total')
+      .parent()
+      .invoke('text')
+      .then(totalText => {
+        const totalAsString = totalText.match(/\$[\d.]+/)[0];
+        const totalValue = parseFloat(totalAsString.replace('$', ''));
+        
+        const expectedProductsTotal = 19.99 + 109.99;
+        
+        expect(totalValue).to.be.at.least(expectedProductsTotal);
+        cy.log(`Cart total verified: ${totalAsString}`);
+      });
   }
 }); 
